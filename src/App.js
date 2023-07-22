fe91474e4b495f4bb12cfc48d365e0fe2604aadd
seemaholiday <seemaholidaydeveloper@gmail.com>
2023-07-22 16:53:01 +0530

    initial commit -m

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 
 function App() {
   const [dateList, setDateList] = useState(
-    [{date:"Mar 1 2020"},
-    {date:"Jan 2 2020"}, {date:"Dec 31 2002"}, 
-
-    // {date:"2/3/2023"}, {date:"2/11/2021"}, {date:"2/2/2016"}, 
-    // {date:"2/5/2000"}, {date:"2/3/2025"}, {date:"2/3/2009"}, 
-    // {date:"5/3/2011"}
+    [{date:"1/2/2020"},
+    {date:"1/1/2020"}, {date:"2/8/2002"}, 
+    {date:"2/3/2023"}, {date:"2/11/2021"}, {date:"2/2/2016"}, 
+    {date:"2/5/2000"}, {date:"2/3/2025"}, {date:"2/3/2009"}, 
+    {date:"5/3/2011"}
   ])
     const [sortList, setSortList] = useState([])
   useEffect(()=>{
